269e34584d8d4813980bee056d1c6029e051cb4c
Reza paidar <reza.paidar@outlook.com>
2023-05-23 18:59:00 +0330

    Update Xss.cs

--- Xss.cs
+++ Xss.cs
@@ -1,1 +1,8 @@
-
+public static class Xss
+{
+    public static bool Check_Mobile_Number(string value)
+    {
+        const string pattern = "09\\d{9}$";
+        return !string.IsNullOrEmpty(value) && Regex.IsMatch(value.Trim(), pattern);
+    }
+}
